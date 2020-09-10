6322748eaf2ccede8d0bd10143cbe704675ea646
Leonardo Frederico <freedyzera@gmail.com>
2020-09-09 21:24:31 -0300

    header total reponsive + dropdown 100% functional

--- dropdown.js
+++ dropdown.js
@@ -1,5 +1,13 @@
 document.querySelector('.lni-menu')
-    .addEventListener('click', function() {
+    .addEventListener('click', function(event) {
+        if (event.target.classList[1] === 'lni-menu'){
+            event.target.classList.remove('lni-menu');
+            event.target.classList.add('lni-close');
+        } else {
+            event.target.classList.remove('lni-close');
+            event.target.classList.add('lni-menu');
+        }
+        console.log(event.target.classList)
         document.querySelector('#navbar')
             .classList.toggle('toggle-nav');
     });
