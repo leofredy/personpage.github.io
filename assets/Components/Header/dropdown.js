a8afe56aeb7e362e868c70ba9021165519d180ab
Leonardo Frederico <freedyzera@gmail.com>
2020-10-08 23:15:36 -0300

    Added animation in nav dropdown mobile

--- assets/Components/Header/dropdown.js
+++ assets/Components/Header/dropdown.js
@@ -1,14 +1,44 @@
-document.querySelector('.lni-menu')
-    .addEventListener('click', function(event) {
-        if (event.target.classList[1] === 'lni-menu'){
-            event.target.classList.remove('lni-menu');
-            event.target.classList.add('lni-close');
-        } else {
-            event.target.classList.remove('lni-close');
-            event.target.classList.add('lni-menu');
-        }
-        document.querySelector('.unfold-header nav')
-            .classList.toggle('toggle');
+class AnimationDropdown {
+    constructor(){
+        this.firstAnimation = true;
+        this.nav = document.querySelector('.unfold-header nav');
+        this.navContent = document.querySelector('.unfold-header nav .container');
+    }
 
-            console.log('presseding')
-    });
+    Animation(event){
+        const element = event.target;
+        const selector = element.classList[1];
+        if (selector === 'lni-menu'){
+            this.Open(element);
+        } else {
+            this.Close(element);
+        }
+    }
+
+    Open(element){
+        element.classList.remove('lni-menu');
+        element.classList.add('lni-close');
+        this.navContent.classList.remove('closed');
+        this.nav.classList.add('toggle');
+
+        clearTimeout(this.DisappearsNav); 
+    }
+
+    Close(element){
+
+        element.classList.remove('lni-close');
+        element.classList.add('lni-menu');
+        this.navContent.classList.add('closed');
+
+        this.DisappearsNav = setTimeout( () => {
+            this.nav.classList.remove('toggle');
+        }, 500)
+    }
+}
+
+const Dropdown = new AnimationDropdown();
+
+document.querySelector('.lni-menu')
+    .addEventListener('click', function(event){
+        Dropdown.Animation(event)
+    })
