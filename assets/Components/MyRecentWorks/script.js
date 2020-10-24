3f439e2a4420042586612862bd717f83084c7527
Leonardo Frederico <freedyzera@gmail.com>
2020-10-23 23:11:00 -0300

    Creating animation hover in the elements Works

--- assets/Components/MyRecentWorks/script.js
+++ assets/Components/MyRecentWorks/script.js
@@ -1,11 +1,25 @@
+const elementWorks = document.querySelectorAll('section.my-recent-works ul li');
+
+elementWorks.forEach(elementWork => {
+    elementWork.addEventListener('mouseenter', watchOverWork);
+    elementWork.addEventListener('mouseleave', watchLeaveWork);
+});
+
 window.addEventListener('load', handleWidthWork);
 window.addEventListener('resize', handleWidthWork);
 
 function handleWidthWork(){
-    const elementWorks = document.querySelectorAll('section.my-recent-works ul li');
 
     elementWorks.forEach(elementWork => {
         elementWork.style.height = `${elementWork.offsetWidth}px`;
         elementWork.children[0].innerHTML = `${elementWork.offsetWidth} x ${elementWork.offsetHeight}`;
+        elementWork.children[1].children[1].style.width = `${elementWork.offsetWidth}px`;
     });
+}
+
+function watchOverWork(event){
+    event.target.children[1].classList.add('modal');
+}
+function watchLeaveWork(event){
+    event.target.children[1].classList.remove('modal');
 }
